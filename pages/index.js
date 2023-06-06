f5b26e97ded9746947e04b082407edeb195f2d21
jeongbeen2 <aceman9508@gmail.com>
2023-06-06 14:13:18 +0900

    index 경로 수정 및 speaker 주석

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import { useRef } from "react";
+// Components
+import Navigator from "@/components/Navigator";
+import Home from "@/components/Home";
+import Main from "@/components/Main";
+import Commitees from "@/components/Commitees";
+import Sponsors from "@/components/Sponsors";
+import Speakers from "@/components/Speakers";
+import Timetable from "@/components/Timetable";
+import Organizer from "@/components/Organizer";
+import Map from "@/components/Map";
+import Footer from "@/components/Footer";
+import { LandingProvider } from "@/components/context/context";
+import { prefix } from "@/components/config/config";
+
+// Style
+import * as SC from "@/components/styles";
+
+const Landing = () => {
+  const scrollRef = useRef(null);
+
+  const handleButtonClick = () => {
+    // 스크롤 위치로 이동하기
+    scrollRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <>
+      <LandingProvider value={{ prefix }}>
+        <SC.Container>
+          <Navigator handleButtonClick={handleButtonClick} />
+          <Main handleButtonClick={handleButtonClick} />
+          <Home />
+          <Commitees />
+          {/* <Sponsors /> */}
+          <Speakers />
+          <Timetable />
+          <Organizer />
+          <section ref={scrollRef}>
+            <Map />
+          </section>
+          <Footer />
+        </SC.Container>
+      </LandingProvider>
+    </>
+  );
+};
+
+export default Landing;
