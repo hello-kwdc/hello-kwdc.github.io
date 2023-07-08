5987e9c6e65c7e80868e863a4cfbd33d2afc4350
unnnyong <unnnyong@gmail.com>
2023-07-09 01:16:53 +0900

    feat: add timetable

--- src/components/Timetable/index.js
+++ src/components/Timetable/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import * as SC from "@/components/styles";
 
-import { TRACK_1, TRACK_2, TRACK_3, TRACK_4 } from "./track.js";
+import { TRACK_여의도, TRACK_강남, TRACK_명동, TRACK_가로수길 } from "./track.js";
 
 const SectionTitle = () => {
   return (
     <SC.SectionTitle style={{ top: "-90px" }}>
       <div style={{ fontSize: "30px", fontWeight: 700 }}>Timetable</div>
       <div className="timetable_title">
-        자세한 시간표는 7월 초 공개될 예정입니다.
+        시간표는 상황에 따라 변경될 수 있습니다.
       </div>
     </SC.SectionTitle>
   );
@@ -20,23 +20,30 @@ const Timetable = () => {
       <SC.SectionTimetable>
         <SC.SectionTimetableContent>
           <SC.SectionTimetableContentWrapper>
-            {[TRACK_1, TRACK_2, TRACK_3, TRACK_4].map((track, trackIndex) => {
+            {[TRACK_여의도, TRACK_강남, TRACK_명동, TRACK_가로수길].map((track, trackIndex) => {
               return (
                 <SC.TimetableEach key={`TRACK_${trackIndex}`}>
                   {trackIndex === 0 ? <SectionTitle /> : null}
-                  <SC.TableTitle>Track {trackIndex + 1}</SC.TableTitle>
-                  {track.map((info, i) => {
+                  <SC.TableTitle>Track {track.name}</SC.TableTitle>
+                  {track.contents.map((info, i) => {
                     return (
                       <React.Fragment key={`TABLE_${i}`}>
                         {info.type === "full" ? (
                           <SC.TableFullContent>
-                            <span>{info.time}</span>
-                            <div>{info.content}</div>
+                            <span style={{ whiteSpace: "nowrap" }}>{info.time}</span>
+                            <div style={{ marginRight: "20px" }}>
+                              <b>{info.speaker}</b><br/>
+                                {info.content.split('\n').map(line => {
+                                  return (
+                                    <span>{line}<br /></span>
+                                  );
+                                })}
+                            </div>
                           </SC.TableFullContent>
                         ) : (
                           <SC.TableEmptyContent>
                             <span>{info.time}</span>
-                            <div>{info.content}</div>
+                            <div>{info.content}<br/><br/></div>
                           </SC.TableEmptyContent>
                         )}
                       </React.Fragment>
